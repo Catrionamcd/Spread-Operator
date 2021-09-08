a7fa6063f69e808fbfd4374d7245114fec2a0085
Catrionamcd <catrionamcd@gmail.com>
2021-09-08 15:19:58 +0000

    Add code to copy an array, an object and part of an array/object

--- the-spread-operator.js
+++ the-spread-operator.js
@@ -4,9 +4,30 @@
  */
 
 // No spread operator
+let arr1 = [1, 2, 3];
+let arr2 = arr1;
+arr2.push(4)
+console.log("2nd Array:", arr2);
+console.log("1st Array:", arr1);
 
 // Copying an array
+let arr3 = [4, 5, 6];
+let arr4 = [...arr3]
+
+arr4.push(7);
+console.log("3rd Array:", arr3);
+console.log("4th Array:", arr4);
 
 // Copying an object
+let obj1 = {a: 1, b: 2, c: 3}
+let obj2 = {...obj1, d: 4}
+let obj3 = {...obj1, b: 5}
+console.log("1st Obj", obj1);
+console.log("2nd Obj", obj2);
+console.log("3rd Obj", obj3)
 
 // Copying only part of an array/object
+let arr5 = [...arr1, {...obj1}, ...arr3, "x", "y", "z"];
+console.log("5th Array:", arr5);
+
+
